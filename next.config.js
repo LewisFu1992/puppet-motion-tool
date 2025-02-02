ca0b19862f1123a07f5822c2eb089cb4cd96ff3b
LewisFu1992 <fushiliu@126.com>
2025-02-02 10:43:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',  // 添加这行来支持静态导出
+  images: {
+    unoptimized: true,  // 添加这行来支持图片静态导出
+  },
 }
 
 module.exports = nextConfig
